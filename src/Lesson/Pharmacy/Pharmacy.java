a85ec43d4e758bea2c613303e1169c856c34c68b
Dyadyk Alexander <a.dyadyk@gmail.com>
2023-12-23 12:39:17 +0300

    Override next & hasNext. Add PharmacyMain.java

--- src/Lesson/Pharmacy/Pharmacy.java
+++ src/Lesson/Pharmacy/Pharmacy.java
@@ -1,14 +1,25 @@
 package Lesson.Pharmacy;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class Pharmacy {
+public class Pharmacy implements Iterator<Component> {
     private int index;
     private List<Component> components;
 
     public Pharmacy() {
         this.index = 0;
         this.components = new ArrayList<>();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < components.size(); // Сравниваем индекс с длиной списка
+    }
+
+    @Override
+    public Component next() {
+        return components.get(index++); // Получаем компонент по индексу, а потом увеличиваем индекс на еденицу
     }
 }
---
+++ src/Lesson/Pharmacy/PharmacyMain.java
@@ -0,0 +1,10 @@
+package Lesson.Pharmacy;
+
+public class PharmacyMain {
+    public static void main(String[] args) {
+        Component pineceline = new Component("Pineceline", "10mg", 100);
+        Component salt = new Component("Salt", "50mg", 13);
+        Component sugar = new Component("Sugar", "24mg", 78);
+        Pharmacy pharmacy1 = new Pharmacy();
+    }
+}
